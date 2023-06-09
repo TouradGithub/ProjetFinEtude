6ca803b4d5febaebf21fac0e2ecebf8e8097bde3
TouradGithub <95051771+TouradGithub@users.noreply.github.com>
2023-06-08 18:19:59 -0700

    add
    add

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -2,7 +2,7 @@
 <title>@yield("title")</title>
 
 <!-- Favicon -->
-<link rel="shortcut icon" href="{{ URL::asset('assets/images/favicon.ico') }}" type="image/x-icon" />
+<link rel="shortcut icon" href="{{ URL::asset('logo.jpg') }}" type="image/x-icon" />
 
 <!-- Font -->
 <link rel="stylesheet"
